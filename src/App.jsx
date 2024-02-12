a347c7850868d6cd8e9e5038c77002509549d98c
FSwebdeveloper <achintahaldar183@gmail.com>
2024-02-12 13:22:49 +0530

    commit all mylogin file

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+function App() {
+    
+
+  const[change, setChange] = useState("");
+
+  function handleChange (e) {
+   setChange({
+    ...change,
+     [e.target.name] : e.target.value
+   })
+  }
+
+  const handleSubmit = (e)=> {
+    e.preventDefault();
+    fetch("/login",{
+    method:"POST",
+    body:JSON.stringify(change),
+    headers:{
+        "Content-Type": "application/json"
+    }
+    })
+    .then((result) => {
+      return result.json()
+    })
+    .then((data) => {
+    console.log(data)
+    });
+
+  }
+
+  
+  return (
+    <div className="container">
+    <form className="form" onSubmit={handleSubmit}>
+      <input type="text" placeholder="Username" name="email" onChange={handleChange}/>
+      <input type="password" placeholder="Password" name="password" onChange={handleChange} />
+      <button type="submit">login</button>
+    </form>
+    </div>
+  );
+}
+
+export default App;
+
